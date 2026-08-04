98d1bac50c35e4387724bbec5125573461680704
DogukanMuhtar <148693057+DogukanMuhtar@users.noreply.github.com>
2026-08-04 13:31:12 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function kayitOL() {
+    const servicesSection = document.querySelector('.services');
+    if (servicesSection) {
+        servicesSection.scrollIntoView({ behavior: 'smooth' });
+    }
+}
